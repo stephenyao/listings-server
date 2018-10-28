eb5945052276322eb6576e87b61ffcb7c034a532
Stephen Yao <stephenryao@gmail.com>
2018-10-28 20:42:34 +1100

    Added Agency name to Listing object

--- Sources/App/Models/Listing.swift
+++ Sources/App/Models/Listing.swift
@@ -6,12 +6,14 @@ struct Listing: Content {
 
 	let price: String
 
-	let imageURL: URL
+  let agencyName: String
+  
+  let imageURL: URL
     
-    static func all() -> [Listing] {
-        let listing = Listing(title: "511 Church Street", price: "$20", imageURL: URL(string: "https://i2.au.reastatic.net/c8975b410f321eb57d7efb199f22bddeba7a0bc89529213f3403de2753a59fea/image2.jpg")!)
-        
-        return [listing]
-    }
+  static func all() -> [Listing] {
+    let listing = Listing(title: "511 Church Street", price: "$20", agencyName: "Ray White", imageURL: URL(string: "https://i2.au.reastatic.net/c8975b410f321eb57d7efb199f22bddeba7a0bc89529213f3403de2753a59fea/image2.jpg")!)
+    
+      return [listing]
+  }
 
 }

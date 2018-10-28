d17a16a72c7b707776c0821badc7169d4c7c3ad2
Stephen Yao <stephenryao@gmail.com>
2018-10-28 20:40:01 +1100

    Renamed ListingSummary -> Listing

--- Sources/App/Models/Listing.swift
+++ Sources/App/Models/Listing.swift
@@ -1,6 +1,6 @@
 import Vapor
 
-struct ListingSummary: Content {
+struct Listing: Content {
 
 	let title: String
 
@@ -8,8 +8,8 @@ struct ListingSummary: Content {
 
 	let imageURL: URL
     
-    static func all() -> [ListingSummary] {
-        let listing = ListingSummary(title: "511 Church Street", price: "$20", imageURL: URL(string: "https://i2.au.reastatic.net/c8975b410f321eb57d7efb199f22bddeba7a0bc89529213f3403de2753a59fea/image2.jpg")!)
+    static func all() -> [Listing] {
+        let listing = Listing(title: "511 Church Street", price: "$20", imageURL: URL(string: "https://i2.au.reastatic.net/c8975b410f321eb57d7efb199f22bddeba7a0bc89529213f3403de2753a59fea/image2.jpg")!)
         
         return [listing]
     }
